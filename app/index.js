e12b2fa80b2cf9cdfe907cfabeb27d8dcc68f4c3
Eduardo Paixão <duduindo@gmail.com>
2019-03-20 15:55:11 -0300

    Libraries added

---
+++ app/index.js
@@ -0,0 +1,8 @@
+
+class App {
+  constructor() {
+    console.log('App Certo')
+  }
+}
+
+export default App
--- devtools.js
+++ devtools.js
@@ -1,27 +1,17 @@
 
+import './app/library/vuejs-v2.6.9.js'
+import './app/library/http-vue-loader-v1.4.0.js'
+import App from './app/index.js'
+
+
+
 chrome.devtools.panels.create('IndexedDB My Admin', '', 'devtools.html', function(panel) {
-	const button = document.querySelector('button');
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+  const app = new Vue({
+    render: h => h('<template></template>')
+  }).$mount('#app')
 
 
-
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    const local = localStorage.getItem('local');
-
-    button.onclick = () => {
-      chrome.tabs.sendMessage(tabs[0].id, 'opa');
-    };
-
-    button.textContent += Date.now();
-
-    if (local) {
-      console.warn('Existe item "local"');
-    } else {
-      localStorage.setItem('local', 'STORAGE');
-    }
-
-
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      console.log(request);
-    });
-  });
+  // });
 });
